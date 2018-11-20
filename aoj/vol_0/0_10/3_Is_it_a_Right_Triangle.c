9e9b69de11eedeab07bfd3d9ea4beee177f7ee26
KitsuKitsu <akira1410.k@gmail.com>
2018-11-21 01:43:27 +0900

    [add]aojやった

--- aoj/vol_0/0_10/3_Is_it_a_Right_Triangle.c
+++ aoj/vol_0/0_10/3_Is_it_a_Right_Triangle.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 
+void sort(int *num);
+
+void sort(int *num){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(num[i] > num[j]){
+                int max = num[j];
+                int min = num[i];
+                num[i] = max;
+                num[j] = min;
+            }
+        }
+    }
+}
 int main()
 {
     int n;
     scanf("%d", &n);
-    for(int i=0;i<n;i++){
-        int num[2];
-        scanf("%d %d %d", &num[0], &num[1], &num[2]);
-        int syahen;
-        for (int j = 0; j<3; j++){
-            if(num[i] > num[j]){
-                
-            }
-        }
+    char * ans[n];
+    for (int i=0; i<n; i++){
+      int num[256];
+      scanf("%d %d %d", &num[0], &num[1], &num[2]);
+      sort(num);
+      if(num[0] > num[1] && num[0] > num[2]){
+          if(num[0]*num[0] == num[1]*num[1] + num[2]*num[2]) {
+            ans[i] = "YES";
+          } else {
+            ans[i] = "NO";
+          }
+      } else {
+          ans[i] = "NO";
+      }
+    }
+    for (int i=0; i<n; i++){
+        printf("%s\n",ans[i]);
     }
     return 0;
 }
